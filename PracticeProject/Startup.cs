23b847f575e700a4893a892b9544a6e74507be06
Archana Pandey <archanapandey1007@gmail.com>
2018-06-28 14:24:24 +0530

    project uploaded

---
+++ PracticeProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PracticeProject.Startup))]
+namespace PracticeProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
